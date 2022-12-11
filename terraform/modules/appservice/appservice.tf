7eb6438f1724863c3be26a865e2fccaec66cfe37
sanjidat <sanjidaorin11@gmail.com>
2022-12-11 13:24:37 +0100

    initial commit

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -2,7 +2,7 @@ resource "azurerm_app_service_plan" "test" {
   name                = "${var.application_type}-appsplan"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-
+  os_type             = "Linux"
   sku {
     tier = "Free"
     size = "F1"
